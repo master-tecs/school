19fec9f2a05fdd0d8d5b17818a94e7baba51b7c8
Abdul <masterawahab@gmail.com>
2025-02-22 15:27:36 -0500

    Removed the route in the api/recommend dir

--- src/app/api/recommend/route.js
+++ src/app/api/recommend/route.js
@@ -1,20 +1,20 @@
-import OpenAI from "openai";
+// import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-export function POST(req, res) {
+// export function POST(req, res) {
 
-const completion = openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    store: true,
-    messages: [
-      {"role": "user", "content": "write a haiku about ai"},
-    ],
-  });
+// const completion = openai.chat.completions.create({
+//     model: "gpt-4o-mini",
+//     store: true,
+//     messages: [
+//       {"role": "user", "content": "write a haiku about ai"},
+//     ],
+//   });
   
-  completion.then((result) => console.log(result.choices[0].message));
+//   completion.then((result) => console.log(result.choices[0].message));
   
-  return new Response("Hello, Next.js!");
-}
+//   return new Response("Hello, Next.js!");
+// }
